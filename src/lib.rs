783d1e9c14637cbf3bc20a3577fba212d44983ef
Keir Lawson <keirlawson@gmail.com>
2019-11-12 22:04:38 +0000

    Add commit all method

--- src/lib.rs
+++ src/lib.rs
@@ -58,9 +58,32 @@ impl Repository {
     //Create and checkout a new local branch
     pub fn create_local_branch(&self, branch_name: &str) -> Result<(), GitError> {
         Command::new("git")
+        .current_dir(&self.location)
         .arg("checkout")
         .arg("-b")
         .arg(branch_name)
+        .output()
+        .map_err(|_| GitError {
+            message: String::from("unable to execute git process"),
+        })
+        .and_then(|output| {
+            if output.status.success() {
+                Ok(())
+            } else {
+                let message =
+                    str::from_utf8(&output.stderr).unwrap_or_else(|_| "unable to decode error");
+                Err(GitError {
+                    message: String::from(message),
+                })
+            }
+        })
+    }
+
+    pub fn commit_all(&self) -> Result<(), GitError> {
+        Command::new("git")
+        .current_dir(&self.location)
+        .arg("commit")
+        .arg("-a")
         .output()
         .map_err(|_| GitError {
             message: String::from("unable to execute git process"),
