37f7c5102d7ff3353ada77f0763aba2adef83808
Keir Lawson <keirlawson@gmail.com>
2019-11-26 19:38:49 +0000

    Clean up ready for release

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::str::FromStr;
-
-fn main() {
-    let url =
-        rustygit::GitUrl::from_str("https://github.com/keirlawson/rusty-git.git").unwrap();
-    let repo = rustygit::Repository::clone(url, "/home/keir/Code/testclone");
-    repo.unwrap();
-    println!("Hello, world!");
-}
