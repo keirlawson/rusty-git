bd2c0e3cb6d815da09b2dce8e3a022430a65200e
gyaur <40827800+gyaur@users.noreply.github.com>
2020-06-19 20:01:14 +0200

    basic git remove and status functionality  (#4)
    * Add remove
    
    * Add remove tests
    
    * Add status
    
    * Add status tests
    
    * More explicit error assertions for remove

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::str;
-use types::{GitUrl, Result, BranchName};
+use types::{BranchName, GitUrl, Result};
 
 pub mod error;
 pub mod types;
@@ -44,7 +44,10 @@ impl Repository {
 
     ///Create and checkout a new local branch
     pub fn create_local_branch(&self, branch_name: &BranchName) -> Result<()> {
-        execute_git(&self.location, &["checkout", "-b", branch_name.value.as_str()])
+        execute_git(
+            &self.location,
+            &["checkout", "-b", branch_name.value.as_str()],
+        )
     }
 
     ///Checkout the specified branch
@@ -53,13 +56,21 @@ impl Repository {
     }
 
     ///Add file contents to the index
-    pub fn add(&self, pathspecs: Vec<&str>) -> Result<()> 
-    {
+    pub fn add(&self, pathspecs: Vec<&str>) -> Result<()> {
         let mut args = pathspecs.clone();
         args.insert(0, "add");
         execute_git(&self.location, args)
     }
 
+    ///Remove file contents from the index
+    pub fn remove(&self, pathspecs: Vec<&str>, force: bool) -> Result<()> {
+        let mut args = pathspecs.clone();
+        args.insert(0, "rm");
+        if force {
+            args.push("-f");
+        }
+        execute_git(&self.location, args)
+    }
     ///Commit all staged files
     pub fn commit_all(&self, message: &str) -> Result<()> {
         execute_git(&self.location, &["commit", "-am", message])
@@ -72,7 +83,10 @@ impl Repository {
 
     ///Push the curent branch to its associated remote, specifying the upstream branch
     pub fn push_to_upstream(&self, upstream: &str, upstream_branch: &BranchName) -> Result<()> {
-        execute_git(&self.location, &["push", "-u", upstream, upstream_branch.value.as_str()])
+        execute_git(
+            &self.location,
+            &["push", "-u", upstream, upstream_branch.value.as_str()],
+        )
     }
 
     ///Add a new remote
@@ -86,17 +100,55 @@ impl Repository {
     }
 
     ///Create a new branch from a start point, such as another local or remote branch
-    pub fn create_branch_from_startpoint(&self, branch_name: &BranchName, startpoint: &str) -> Result<()> {
-        execute_git(&self.location, &["checkout", "-b", branch_name.to_string().as_str(), startpoint])
+    pub fn create_branch_from_startpoint(
+        &self,
+        branch_name: &BranchName,
+        startpoint: &str,
+    ) -> Result<()> {
+        execute_git(
+            &self.location,
+            &[
+                "checkout",
+                "-b",
+                branch_name.to_string().as_str(),
+                startpoint,
+            ],
+        )
     }
 
     ///List local branches
     pub fn list_branches(&self) -> Result<Vec<String>> {
-        execute_git_fn(&self.location, &["branch", "--format=%(refname:short)"], |output| {
-            output.lines().map(|line| line.to_owned()).collect()
-        })
+        execute_git_fn(
+            &self.location,
+            &["branch", "--format=%(refname:short)"],
+            |output| output.lines().map(|line| line.to_owned()).collect(),
+        )
     }
 
+    ///List files added to staging area
+    pub fn list_added(&self) -> Result<Vec<String>> {
+        git_status(&self, "A")
+    }
+
+    ///List all modified files
+    pub fn list_modified(&self) -> Result<Vec<String>> {
+        git_status(&self, " M")
+    }
+
+    ///List all untracked files
+    pub fn list_untracked(&self) -> Result<Vec<String>> {
+        git_status(&self, "??")
+    }
+}
+
+fn git_status(repo: &Repository, prefix: &str) -> Result<Vec<String>> {
+    execute_git_fn(&repo.location, &["status", "-s"], |output| {
+        output
+            .lines()
+            .filter(|line| line.starts_with(&prefix))
+            .map(|line| line[3..].to_owned())
+            .collect()
+    })
 }
 
 fn execute_git<I, S, P>(p: P, args: I) -> Result<()>
@@ -108,13 +160,12 @@ where
     execute_git_fn(p, args, |_| ())
 }
 
-
 fn execute_git_fn<I, S, P, F, R>(p: P, args: I, process: F) -> Result<R>
 where
     I: IntoIterator<Item = S>,
     S: AsRef<OsStr>,
     P: AsRef<Path>,
-    F: Fn(&str) -> R
+    F: Fn(&str) -> R,
 {
     let output = Command::new("git").current_dir(p).args(args).output();
 
@@ -128,9 +179,9 @@ where
         } else {
             if let Ok(stdout) = str::from_utf8(&output.stdout) {
                 if let Ok(stderr) = str::from_utf8(&output.stderr) {
-                    Err(GitError::GitError{
+                    Err(GitError::GitError {
                         stdout: stdout.to_owned(),
-                        stderr: stderr.to_owned()
+                        stderr: stderr.to_owned(),
                     })
                 } else {
                     Err(GitError::Undecodable)
@@ -138,7 +189,6 @@ where
             } else {
                 Err(GitError::Undecodable)
             }
-
         }
     })
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,8 +1,9 @@
 use rustygit::Repository;
-use tempfile;
+use std::fs::File;
+use std::io::Write;
 use std::process::Command;
 use std::str;
-use std::fs::File;
+use tempfile;
 
 #[test]
 fn test_init() {
@@ -10,7 +11,11 @@ fn test_init() {
 
     Repository::init(&dir).unwrap();
 
-    let output = Command::new("git").current_dir(&dir).args(&["rev-parse", "--is-inside-work-tree"]).output().unwrap();
+    let output = Command::new("git")
+        .current_dir(&dir)
+        .args(&["rev-parse", "--is-inside-work-tree"])
+        .output()
+        .unwrap();
 
     assert!(output.status.success());
     assert_eq!(str::from_utf8(&output.stdout).unwrap().trim(), "true");
@@ -24,9 +29,13 @@ fn test_add_single() {
 
     File::create(dir.as_ref().join("somefile")).unwrap();
 
-    repo.add(vec!("somefile")).unwrap();
+    repo.add(vec!["somefile"]).unwrap();
 
-    let output = Command::new("git").current_dir(&dir).args(&["ls-files"]).output().unwrap();
+    let output = Command::new("git")
+        .current_dir(&dir)
+        .args(&["ls-files"])
+        .output()
+        .unwrap();
 
     assert!(str::from_utf8(&output.stdout).unwrap().contains("somefile"));
 }
@@ -40,14 +49,18 @@ fn test_add_multiple() {
     File::create(dir.as_ref().join("somefile")).unwrap();
     File::create(dir.as_ref().join("anotherfile")).unwrap();
 
+    repo.add(vec!["somefile", "anotherfile"]).unwrap();
 
-    repo.add(vec!("somefile", "anotherfile")).unwrap();
-
-
-    let output = Command::new("git").current_dir(&dir).args(&["ls-files"]).output().unwrap();
+    let output = Command::new("git")
+        .current_dir(&dir)
+        .args(&["ls-files"])
+        .output()
+        .unwrap();
 
     assert!(str::from_utf8(&output.stdout).unwrap().contains("somefile"));
-    assert!(str::from_utf8(&output.stdout).unwrap().contains("anotherfile"));
+    assert!(str::from_utf8(&output.stdout)
+        .unwrap()
+        .contains("anotherfile"));
 }
 
 #[test]
@@ -58,11 +71,189 @@ fn test_commit_all() {
 
     File::create(dir.as_ref().join("somefile")).unwrap();
 
-    repo.add(vec!("somefile")).unwrap();
+    repo.add(vec!["somefile"]).unwrap();
 
     repo.commit_all("some commit message").unwrap();
-    
-    let output = Command::new("git").current_dir(&dir).args(&["log"]).output().unwrap();
 
-    assert!(str::from_utf8(&output.stdout).unwrap().contains("some commit message"));
+    let output = Command::new("git")
+        .current_dir(&dir)
+        .args(&["log"])
+        .output()
+        .unwrap();
+
+    assert!(str::from_utf8(&output.stdout)
+        .unwrap()
+        .contains("some commit message"));
+}
+
+#[test]
+fn test_remove_uncommitted_single() {
+    let dir = tempfile::tempdir().unwrap();
+
+    let repo = Repository::init(&dir).unwrap();
+
+    File::create(dir.as_ref().join("somefile")).unwrap();
+
+    repo.add(vec!["somefile"]).unwrap();
+    let result = repo.remove(vec!["somefile"], false);
+
+    assert!(result.is_err());
+}
+
+#[test]
+fn test_remove_uncommitted_single_force() {
+    let dir = tempfile::tempdir().unwrap();
+
+    let repo = Repository::init(&dir).unwrap();
+
+    File::create(dir.as_ref().join("somefile")).unwrap();
+
+    repo.add(vec!["somefile"]).unwrap();
+    repo.remove(vec!["somefile"], true).unwrap();
+
+    let output = Command::new("git")
+        .current_dir(&dir)
+        .args(&["ls-files"])
+        .output()
+        .unwrap();
+
+    assert!(!str::from_utf8(&output.stdout).unwrap().contains("somefile"));
+}
+
+#[test]
+fn test_remove_committed_single() {
+    let dir = tempfile::tempdir().unwrap();
+
+    let repo = Repository::init(&dir).unwrap();
+
+    File::create(dir.as_ref().join("somefile")).unwrap();
+
+    repo.add(vec!["somefile"]).unwrap();
+    repo.commit_all("some msg").unwrap();
+    repo.remove(vec!["somefile"], false).unwrap();
+
+    let output = Command::new("git")
+        .current_dir(&dir)
+        .args(&["ls-files"])
+        .output()
+        .unwrap();
+
+    assert!(!str::from_utf8(&output.stdout).unwrap().contains("somefile"));
+}
+
+#[test]
+fn test_remove_commited_multiple() {
+    let dir = tempfile::tempdir().unwrap();
+
+    let repo = Repository::init(&dir).unwrap();
+
+    File::create(dir.as_ref().join("somefile")).unwrap();
+    File::create(dir.as_ref().join("anotherfile")).unwrap();
+
+    repo.add(vec!["somefile", "anotherfile"]).unwrap();
+    repo.commit_all("some msg").unwrap();
+
+    repo.remove(vec!["somefile", "anotherfile"], false).unwrap();
+
+    let output = Command::new("git")
+        .current_dir(&dir)
+        .args(&["ls-files"])
+        .output()
+        .unwrap();
+
+    assert!(!str::from_utf8(&output.stdout).unwrap().contains("somefile"));
+    assert!(!str::from_utf8(&output.stdout)
+        .unwrap()
+        .contains("anotherfile"));
+}
+
+#[test]
+fn test_remove_uncommited_multiple() {
+    let dir = tempfile::tempdir().unwrap();
+
+    let repo = Repository::init(&dir).unwrap();
+
+    File::create(dir.as_ref().join("somefile")).unwrap();
+    File::create(dir.as_ref().join("anotherfile")).unwrap();
+
+    repo.add(vec!["somefile", "anotherfile"]).unwrap();
+    let result = repo.remove(vec!["somefile", "anotherfile"], false);
+
+    assert!(result.is_err());
+}
+
+#[test]
+fn test_remove_uncommited_multiple_force() {
+    let dir = tempfile::tempdir().unwrap();
+
+    let repo = Repository::init(&dir).unwrap();
+
+    File::create(dir.as_ref().join("somefile")).unwrap();
+    File::create(dir.as_ref().join("anotherfile")).unwrap();
+
+    repo.add(vec!["somefile", "anotherfile"]).unwrap();
+    repo.remove(vec!["somefile", "anotherfile"], true).unwrap();
+
+    let output = Command::new("git")
+        .current_dir(&dir)
+        .args(&["ls-files"])
+        .output()
+        .unwrap();
+
+    assert!(!str::from_utf8(&output.stdout).unwrap().contains("somefile"));
+    assert!(!str::from_utf8(&output.stdout)
+        .unwrap()
+        .contains("anotherfile"));
+}
+
+#[test]
+fn test_list_added() {
+    let dir = tempfile::tempdir().unwrap();
+
+    let repo = Repository::init(&dir).unwrap();
+
+    File::create(dir.as_ref().join("somefile")).unwrap();
+    File::create(dir.as_ref().join("anotherfile")).unwrap();
+
+    repo.add(vec!["somefile", "anotherfile"]).unwrap();
+
+    let output = repo.list_added().unwrap();
+
+    assert!(output.contains(&String::from("somefile")));
+    assert!(output.contains(&String::from("anotherfile")));
+}
+
+#[test]
+fn test_list_untracked() {
+    let dir = tempfile::tempdir().unwrap();
+
+    let repo = Repository::init(&dir).unwrap();
+
+    File::create(dir.as_ref().join("somefile")).unwrap();
+    File::create(dir.as_ref().join("anotherfile")).unwrap();
+
+    let output = repo.list_untracked().unwrap();
+
+    assert!(output.contains(&String::from("somefile")));
+    assert!(output.contains(&String::from("anotherfile")));
+}
+
+#[test]
+fn test_list_modified() {
+    let dir = tempfile::tempdir().unwrap();
+
+    let repo = Repository::init(&dir).unwrap();
+
+    let mut file1 = File::create(dir.as_ref().join("somefile")).unwrap();
+    let mut file2 = File::create(dir.as_ref().join("anotherfile")).unwrap();
+    repo.add(vec!["somefile", "anotherfile"]).unwrap();
+    repo.commit_all("some msg").unwrap();
+
+    file1.write(b"Hello there!").unwrap();
+    file2.write(b"General Kenobi").unwrap();
+
+    let output = repo.list_modified().unwrap();
+
+    assert!(output.contains(&String::from("somefile")));
+    assert!(output.contains(&String::from("anotherfile")));
 }
