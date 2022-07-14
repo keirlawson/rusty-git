dd3d561129f97fca4b58af8aa9c91415bf4d5192
danielhaap83 <73883903+danielhaap83@users.noreply.github.com>
2022-07-14 13:31:25 +0200

    new user functions utils

--- src/lib.rs
+++ src/lib.rs
@@ -156,6 +156,26 @@ impl Repository {
         };
         execute_git_fn(&self.location, args, |output| output.trim().to_owned())
     }
+
+    /// Execute user defined command
+    pub fn utils<I, S>(&self, args: I) -> Result<()>
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        execute_git(&self.location, args)
+    }
+
+    /// Execute user defined command with fun
+    pub fn utils_fun<I, S>(&self, args: I) -> Result<Vec<String>>
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        execute_git_fn(&self.location, args, |output| {
+            output.lines().map(|line| line.to_owned()).collect()
+        })
+    }
 }
 
 fn git_status(repo: &Repository, prefix: &str) -> Result<Vec<String>> {
